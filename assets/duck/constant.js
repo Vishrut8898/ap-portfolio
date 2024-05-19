892ca5b5c30240d61e3b8a2a57168703e3c4caed
Vishrut8898 <vishrut9869@gmail.com>
2024-05-20 01:51:08 +0530

    feat: Initial Commit

---
+++ assets/duck/constant.js
@@ -0,0 +1,200 @@
+export const shotsContent = [
+    {
+        name: "Deadpool 2",
+        desc: [
+            'Added muzzal flash and blood.',
+            'Composite the fire on window and integration of fire.',
+            'Composite the cg hand.'
+        ]
+    },
+    {
+        name: "Deadpool 2",
+        desc: [
+            "Added muzzal flash and blood.",
+            "Added Fx shield and integration of shield on Kyle.",
+            "Composite the cg hand."
+        ]
+    },
+    {
+        name: "Dune",
+        desc: [
+            "Keying",
+            "CG vehicle integration.",
+            "Composite the cg characters.",
+            "Composite bg extension."
+        ]
+    },
+    {
+        name: "Hobbs & Shaw",
+        desc: [
+            "Keying",
+            "Bg replacement."
+        ]
+    },
+    {
+        name: "Mayday",
+        desc: [
+            "Keying",
+            "Added cg plane front part."
+        ]
+    },
+    {
+        name: "Mayday",
+        desc: [
+            "Keying",
+            "Added dirt layer and create glass look."
+        ]
+    },
+    {
+        name: "Pacific Rim 2",
+        desc: [
+            "Composite the graphics"
+        ]
+    },
+    {
+        name: "Robot 2",
+        desc: [
+            "Sky replacement and integration.",
+            "Composite cg wings and birds.",
+            "Composite robot 2.0",
+            "Added stadium light,crowd and destruction.",
+        ]
+    },
+    {
+        name: "Robot 2",
+        desc: [
+            "Sky replacement and integration.",
+            "Composite bird and robot ball.",
+            "Added stadium light and muzzal flash.",
+            "Added dust and balanced look's."
+        ]
+    },
+    {
+        name: "The School for Good & Evil (2022)",
+        desc: [
+            "Stich comp",
+            "Composite fx particles.",
+            "Added source of fx on hand."
+        ]
+    },
+    {
+        name: "The School for Good & Evil (2022)",
+        desc: [
+            "Full cg comp."
+        ]
+    },
+    {
+        name: "The School for Good & Evil (2022)",
+        desc: [
+            "Fx Particles comp.",
+        ]
+    },
+    {
+        name: "The School for Good & Evil (2022)",
+        desc: [
+            "Fx particles comp.",
+            "Stich comp."
+        ]
+    },
+    {
+        name: "The King",
+        desc: [
+            "Keying",
+            "Added crowd.",
+            "Added cg trees and tents"
+        ]
+    },
+    {
+        name: "The King",
+        desc: [
+            "Crowd Multiplications"
+        ]
+    },
+    {
+        name: "The King",
+        desc: [
+            "Keying",
+            "Added cg crowd."
+        ]
+    },
+    {
+        name: "The Matrix",
+        desc: [
+            "Full cg comp."
+        ]
+    },
+    {
+        name: "The Matrix",
+        desc: [
+            "Keying",
+            "Bg replacement.",
+            "Added light effects.",
+            "Composite cg elements."
+        ]
+    },
+    {
+        name: "The Matrix",
+        desc: [
+            "Keying",
+            "Bg replacement.",
+            "Added light effects.",
+            "Composite cg elements."
+        ]
+    },
+    {
+        name: "The School for Good & Evil (2022)",
+        desc: [
+            "Keying",
+            "Composite Cg sword.",
+            "Bg replacement.",
+            "Added cg candle lamp stand."
+        ]
+    },
+    {
+        name: "The School for Good & Evil (2022)",
+        desc: [
+            "Finger allignments.",
+            "Composite fx bloods."
+        ]
+    },
+    {
+        name: "The School for Good & Evil (2022)",
+        desc: [
+            "Added cg flowers.",
+        ]
+    },
+    {
+        name: "The School for Good & Evil (2022)",
+        desc: [
+            "Keying",
+            "Bg replacement.",
+            "Added cg candle lamp stand."
+        ]
+    },
+    {
+        name: "Triple Frontier",
+        desc: [
+            "Keying",
+            "Bg replacements"
+        ]
+    },
+    {
+        name: "Venom 2",
+        desc: [
+            "Composite cg snake."
+        ]
+    },
+    {
+        name: "Venom 2",
+        desc: [
+            "Composite cg venom.",
+            "Added integration on venom."
+        ]
+    },
+    {
+        name: "",
+        desc: [
+            ""
+        ]
+    },
+];
---
+++ assets/js/script.js
@@ -0,0 +1,90 @@
+import { shotsContent } from "../duck/constant.js"
+
+$(document).ready(function () {
+  // Logic for opening navbar
+  $('.hamburger').click(() => {
+    $("nav").slideToggle("500");
+  })
+
+  // Logic for adding active css on nav links
+  let prevActiveLink = null;
+  $("nav li a").click((e) => {
+    if((prevActiveLink)?.hasClass('active')) {
+      $(prevActiveLink).removeClass('active');
+    }
+
+    prevActiveLink = $(e.target).parent();
+    $(e.target).parent().addClass('active');
+
+
+    if($(window).innerWidth() <= 768) {
+      $('nav').slideToggle('500')
+    }
+  })
+
+  // Logic for opening shots modal
+  $('.compositing-shots li figure').click((e) => {
+    const imgSrc = $(e.target).find('img').attr('src');
+    const elemIndex = $(e.target).parent().index();
+
+    let liHtml = '';
+    shotsContent[elemIndex].desc.forEach(elem => {
+      liHtml += `<li>${elem}</li>`
+    });
+
+    const modalHtml = `
+    <section class="shots-modal">
+      <div class="backdrop close-shots-modal"></div>
+      <div class="modal-content">
+        <span class="close-shots-modal" title="Close">
+          <i class="fa-solid fa-xmark"></i>
+        </span>
+        <h2>${shotsContent[elemIndex].name}</h2>
+        <ul>${liHtml}</ul>
+        <figure class="modal-image">
+            <img src="${imgSrc}" alt="modal-image">
+        </figure>
+      </div>
+    </section>
+    `;
+
+    $('body main').append(modalHtml);
+
+    $('.close-shots-modal').click(() => {
+        $('.shots-modal').fadeOut(500, () => {
+          $('.shots-modal').remove();
+      });
+    })
+  });
+
+  // Logic to open gallery modal
+  $('.view-gallery').click(e => {
+    const modalHtml = `
+    <section class="gallery-modal">
+      <div class="backdrop close-gallery"></div>
+      <div class="gallery-modal-content">
+        <figure>
+            <img src="./assets/images/collage-3.jpg" alt="gallery collage">
+        </figure>
+      </div>
+    </section>
+    `;
+
+    $('body main').append(modalHtml);
+
+    $('.close-gallery').click(() => {
+        $('.gallery-modal').fadeOut(400, () => {
+          $('.gallery-modal').remove();
+      });
+    });
+  });
+
+  // Logic to add bg color to header on certain scroll
+  $(document).scroll(() => {
+    if($(window).scrollTop() >= 200) {
+      $('header').addClass('header-bg');
+    } else {
+      $('header').removeClass('header-bg');
+    }
+  })
+});
